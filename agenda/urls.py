dcc994b35d3821d93e6d2f563c6ba4421e323867
Pedro Alves Estevam Magaieski <46140459877@>
2025-04-04 11:12:59 -0300

    correção rotas

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/servicos/', view=views.read_servicos, name="Lista todos os serviços disponíveis"),
+    path('api/servicos', view=views.read_servicos, name="Lista todos os serviços disponíveis"),
     path('api/servicos/<int:pk>', view=views.read_servico, name="Detalhes de um serviço específico"),
     path('api/servicos/', view=views.create_servico, name="Cria um novo serviço"),
     path('api/agendamentos', view=views.read_agendamentos, name="Lista todos os agendamentos"),
     path('api/agendamentos/<int:pk>', view=views.read_agendamento, name="Detalhes de um agendamento"),
-    path('api/agendamentos', view=views.create_agendamento, name=" Cria novo agendamento"),
+    path('api/agendamentos/', view=views.create_agendamento, name=" Cria novo agendamento"),
 ]
--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/register', view=views.registration, name="register user"),
+    path('auth/register/', view=views.registration, name="register user"),
     path('auth/login/', view=views.login, name="login user"),
     path('auth/profile/', view=views.get_user_profile, name="protected user profile"),
     path('auth/profile/', view=views.patch_user_profile, name="protected updade profile"),
